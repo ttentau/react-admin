5dd2cb9f1453b9a15ef4718ac7eca62c8d5d4960
ttentau <ttentau@163.com>
2019-02-09 03:46:52 +0800

    txj

--- src/App.js
+++ src/App.js
@@ -1,28 +1,126 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.less'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import {
+    Layout, Menu, Breadcrumb, Icon, Dropdown, Avatar, Badge
+} from 'antd'
+
+const {
+    Header, Content, Sider,
+} = Layout
+
+const SubMenu = Menu.SubMenu
+
+class SiderDemo extends React.Component {
+    state = {
+        collapsed: false,
+    }
+
+    onCollapse = (collapsed) => {
+        // console.log(collapsed)
+        this.setState({collapsed})
+    }
+
+    render() {
+        const menu = (
+            <Menu>
+                <Menu.Item>
+                    <Icon type="bell" className=''/>
+                    <span>个人中心</span>
+                </Menu.Item>
+                <Menu.Item>
+                    <Icon type="setting" className=''/>
+                    <span>个人设置</span>
+                </Menu.Item>
+                <Menu.Divider />
+                <Menu.Item>
+                    <Icon type="logout" className=''/>
+                    <span>退出登录</span>
+                </Menu.Item>
+            </Menu>
+        )
+
+        return (
+            <Layout style={{minHeight: '100vh'}}>
+                <Header style={{background: '#fff', padding: 0, position: 'fixed', zIndex: 1, width: '100%'}}>
+                    <div className='header-left'>
+                        <div className="logo mr20p">
+                            个人博客
+                        </div>
+                        <Breadcrumb style={{margin: '16px 0'}}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </div>
+                    <div className="header-right">
+                        <Dropdown overlay={menu}>
+                            <span className="ant-dropdown-link d-flex align-items-center">
+                                <Avatar icon="user"/>
+                                <span className='ml10p'>TTentau</span>
+                            </span>
+                        </Dropdown>
+
+                        <Badge count={25} className='mr40p ml30p'>
+                            <Icon type="bell" className='f20'/>
+                        </Badge>
+                    </div>
+                </Header>
+                <Layout style={{marginTop: 64}}>
+                    <Sider
+                        style={{overflow: 'auto', height: '100vh', position: 'fixed', left: 0}}
+                        collapsible
+                        collapsed={this.state.collapsed}
+                        onCollapse={this.onCollapse}
+                    >
+                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                            <Menu.Item key="1">
+                                <Icon type="pie-chart"/>
+                                <span>Option 1</span>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Icon type="desktop"/>
+                                <span>Option 2</span>
+                            </Menu.Item>
+                            <SubMenu
+                                key="sub1"
+                                title={<span><Icon type="user"/><span>User</span></span>}
+                            >
+                                <Menu.Item key="3">Tom</Menu.Item>
+                                <Menu.Item key="4">Bill</Menu.Item>
+                                <Menu.Item key="5">Alex</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub2"
+                                title={<span><Icon type="team"/><span>Team</span></span>}
+                            >
+                                <Menu.Item key="6">Team 1</Menu.Item>
+                                <Menu.Item key="8">Team 2</Menu.Item>
+                            </SubMenu>
+                            <Menu.Item key="9">
+                                <Icon type="file"/>
+                                <span>File</span>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Content style={{padding: '0 16px', marginLeft: this.state.collapsed ? 80 : 200}}>
+                        <div style={{padding: 24, background: '#fff'}}>
+                            Bill is a cat.
+                        </div>
+                    </Content>
+                </Layout>
+            </Layout>
+        )
+    }
 }
 
-export default App;
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <SiderDemo/>
+            </div>
+        )
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/less/index.less'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter , Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={App}></Route>
+        </Switch>
+    </BrowserRouter>
+), document.body)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
+
+//热更新
+if (module.hot) {
+    module.hot.accept()
+}
